5d2494956ff0695aadebed66ebbeff07a82db319
Ross Mauck <ross@rossmauck.com>
2018-10-12 17:49:46 -0700

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,184 +1,225 @@
+// TO DO: 20181009
 
-//colors
-let red = "#990000";
-let ocean = "#009999"
-let cyan = "#0ebaba";
-let lime = "#0eba0e";
-//end colors
+// Use 180 degree rotation matrix to choose where to spawn new enemies when the snake eats
+//  this may requre adding parameters tto the enemy class
 
-let snake;
-let rez = 1;
-let food;
-let w;
-let h;
-let spawntimer = 0;
-let fps = 30;
-let foodxsize = 20;
-let foodysize = 20;
-let isFoodSpecial = false;
-let derandomizer = 0;
+class Enemy {
 
-let unit;
-var count;
-var enemies = [];
-var index = 0;
-let inputDelay = 0;
-let end = false;
-var survival = 0;
-// an HTML range slider
-var slider;
-var sliderDifficulty;
-
-var canvas;
-
-function setup() {
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
-  //createCanvas(width, height);
-  resetSketch();
-  var button = createButton('reset');
-  button.mousePressed(resetSketch);
-  // Set up slider with range between 0 and 255 with starting value of 127
-  slider = createSlider(0, 255, 200);
-  slider.position(25, 25);
-  button.position(25, height - 25);
-  setInterval(function(){survival++;}, 1000);
-}
-
-function resetSketch() {
-  end = false;
-  enemies = [];
-  w = floor(width / rez);
-  h = floor(height / rez);
-  unit = floor(width / 2.0);
-  frameRate(fps);
-  snake = new Snake();
-  spawnFood();
-  index = 0;
-
-  // Create Array of Enemies
-  noStroke();
-  // var wideCount = width / unit;
-  // var highCount = height / unit;
-  // count = wideCount * highCount;
-  // index for each enemy
-  
-  for (var mu = 0; mu < 2; mu++) {
-    enemies[index++] = new Enemy(index);
-  }
-}
-
-function spawnFood() {
-  if(random(derandomizer, 20) > 17){
-    isFoodSpecial = true;
-    derandomizer = 0;
-  } else{
-    isFoodSpecial = false;
-    derandomizer++;
-  }
-	foodLocation();
-}
-
-function foodLocation() {
-  let x = (10/rez)*floor(random(1, w/(10/rez)));
-  let y = (10/rez)*floor(random(1, h/(10/rez)));
-  food = createVector(x, y);
-}
-
-function keyPressed() {
-  if (inputDelay == 0) {
-    inputDelay = 1;
-    if (keyCode === LEFT_ARROW && snake.xdir != 10) {
-      snake.setDir(-10, 0);
-    } else if (keyCode === RIGHT_ARROW && snake.xdir != -10) {
-      snake.setDir(10, 0);
-    } else if (keyCode === DOWN_ARROW && snake.ydir != -10) {
-      snake.setDir(0, 10);
-    } else if (keyCode === UP_ARROW && snake.ydir != 10) {
-      snake.setDir(0, -10);
-    } else if (key == ' ') {
-      //resetSketch();
-    }
-  }
-}
-
-function draw() {
-  textSize(20);
-  if (end){
-    background(slider.value());
-    textAlign(floor(width/2));
-    textSize(150);
-    text("Game Over",10,400);
-    textSize(100)
-    if (index-2 == 0){
-      fill(red);
-      //textAlign(CENTER);
-      text("Final Score: "+0,10,550);
-    }
-    else{
-      fill(red);
-      text("Final Score: "+(index-2),10,550);
-    }
-    if (key == ' ') {
-      end = false;
-      resetSketch();
-    }
-  }
-  
-  //regular draw
-  else{
-    scale(rez);
-    background(slider.value());
-
-    // Snake
-    if (snake.eat(food)) {
-      spawnFood();
-      index++;
-      enemies.push(new Enemy(index));
-    }
-    snake.update();
-    snake.show();
-
-    // Enemies
-    enemies.forEach(enemy => {
-      enemy.show();
-      enemy.move();
-      enemy.update();
-      if (snake.endGame(enemy)){
-            end = true;
-          }
-
-    })
-
-    // Food
-    noStroke();
-    if(isFoodSpecial){
-      fill(cyan);
-    } else {
-    fill("#0eba0e");
-    }
-    rect(food.x, food.y, foodxsize, foodysize);
-    text("food", food.x + 18, food.y + 20, 50, 50);
-
-    // update the input pause until next frame
-    inputDelay = 0;
-    fill(0);
-    textSize(40);
-    //20181011 daniel 
-    //score thing in top left  
-    if (index-2 == 0){
+  constructor(index) {
+    this.x = floor(random(w));
+    this.y = floor(random(h));
     
-    text("Score: "+0,width-155,40);
-}
-    else{
-    text("Score: "+(index-2), width-155,40)
+    this.spawnXDiff = this.x - snake.body[0].x;
+    this.spawnYDiff = this.y - snake.body[0].y;
+
+    this.forbiddenRange = 200;
+    //check if the enemy is too close to the snake
+    if (abs(this.spawnXDiff) < this.forbiddenRange && abs(this.spawnYDiff) < this.forbiddenRange) {
+      if (abs(this.spawnXDiff) > abs(this.spawnYDiff)) {
+        this.xIsPreferredAxis = true;
+      } else this.xIsPreferredAxis = false;
+      if (this.xIsPreferredAxis) {
+        if (this.spawnXDiff > 0) {
+          //wants to move to the right
+          if (this.x + this.forbiddenRange < w) {
+            //moving to the right
+            this.x += this.forbiddenRange;
+          } 
+          else if (this.spawnYDiff > 0) {
+            //tried moving to the right, but ooB. Upwards is preferred.
+            if (this.y + this.forbiddenRange < h) {
+              //moving up
+							this.y += this.forbiddenRange;
+            }
+            else {
+              //tried moving up, but out of bounds, moving downwards.
+              this.y -= this.forbiddenRange;
+            }
+          }
+          else {
+            //tried moving to the right, but ooB. Downwards is preferred.
+            if (this.y - this.forbiddenRange > 0) {
+              //moving up
+							this.y -= this.forbiddenRange;
+            }
+            else {
+              //tried moving down, but out of bounds, moving upwards.
+              this.y += this.forbiddenRange;
+            }
+          }
+        }
+        else {
+          //wants to move to the left
+          if (this.x - this.forbiddenRange > 0) {
+            //moving to the left
+            this.x -= this.forbiddenRange;
+          } 
+          else if (this.spawnYDiff > 0) {
+            //tried moving to the left, but ooB. Upwards is preferred.
+            if (this.y + this.forbiddenRange < h) {
+              //moving up
+							this.y += this.forbiddenRange;
+            }
+            else {
+              //tried moving up, but out of bounds, moving downwards.
+              this.y -= this.forbiddenRange;
+            }
+          }
+          else {
+            //tried moving to the left, but ooB. Downwards is preferred.
+            if (this.y - this.forbiddenRange > 0) {
+              //moving up
+							this.y -= this.forbiddenRange;
+            }
+            else {
+              //tried moving down, but out of bounds, moving upwards.
+              this.y += this.forbiddenRange;
+            }
+          }
+        }
+      }
+      else{
+        if (this.spawnYDiff > 0) {
+          //wants to move up
+          if (this.y + this.forbiddenRange < h) {
+            //moving up
+            this.y += this.forbiddenRange;
+          } 
+          else if (this.spawnXDiff > 0) {
+            //tried moving up, but ooB. right is preferred.
+            if (this.x + this.forbiddenRange < w) {
+              //moving right
+							this.x += this.forbiddenRange;
+            }
+            else {
+              //tried moving right, but out of bounds, moving left.
+              this.x -= this.forbiddenRange;
+            }
+          }
+          else {
+            //tried moving up, but ooB. left is preferred.
+            if (this.x - this.forbiddenRange > 0) {
+              //moving left
+							this.x -= this.forbiddenRange;
+            }
+            else {
+              //tried moving left, but out of bounds, moving right.
+              this.x += this.forbiddenRange;
+            }
+          }
+        }
+        else {
+          //wants to move down
+          if (this.y - this.forbiddenRange > 0) {
+            //moving down
+            this.y -= this.forbiddenRange;
+          } 
+          else if (this.spawnXDiff > 0) {
+            //tried moving down, but ooB. right is preferred.
+            if (this.x + this.forbiddenRange < w) {
+              //moving right
+							this.x += this.forbiddenRange;
+            }
+            else {
+              //tried moving right, but out of bounds, moving left.
+              this.x -= this.forbiddenRange;
+            }
+          }
+          else {
+            //tried moving to the down, but ooB. left is preferred.
+            if (this.x - this.forbiddenRange > 0) {
+              //moving up
+							this.x -= this.forbiddenRange;
+            }
+            else {
+              //tried moving left, but out of bounds, moving right.
+              this.x += this.forbiddenRange;
+            }
+          }
+        }
+      }
+    }
+    this.speed = floor(random(1, 10));
+    this.color = color(186, 14, 14);
+    this.direction = 0;
+    this.chaseTimer = 0;
+    this.index = index;
+  }
+
+  // Runs every frame
+  update() {
+    // Boundary Conditions
+    // Check if snake has hit edge of unit boundaries
+    if (this.x > w || this.x < 0) {
+      if (this.direction == 1) {
+        this.direction = 2;
+        this.move();
+      } else {
+        this.direction = 1;
+        this.move();
+      }
+    }
+    // Check if Enemy has hit the y boundary of the unit
+    if (this.y > h || this.y < 0) {
+      if (this.direction == 3) {
+        this.direction = 4;
+        this.move();
+      } else {
+        this.direction = 3;
+        this.move();
+      }
+    }
+    // Update the x direction
+    //this.x = this.x + this.xDir;
+    //this.y = this.y + this.yDir;
+    if (this.chaseTimer > 60) {
+      // Reset chase timer to T-minus 30-90 frames
+      this.chaseTimer = floor(random(-30, 30));
+
+      // Calculate distance between enemy and snake head
+      this.xDiff = this.x - snake.body[0].x;
+      this.yDiff = this.y - snake.body[0].y;
+      // Update the enemy direction based on the sign of Diff for x or y
+      if (abs(this.xDiff) > abs(this.yDiff)) {
+        if (this.xDiff >= 0) {
+          this.direction = 1;
+        } else {
+          this.direction = 2;
+        }
+      } else {
+        if (this.yDiff >= 0) {
+          this.direction = 3;
+        } else {
+          this.direction = 4;
+        }
+      }
+    } else {
+      // Iterate chase timer
+      this.chaseTimer++;
+    }
+  }
+
+  // Custom method for showing the Enemy
+  show() {
+    fill(this.color);
+    noStroke();
+    // Enemy text  
+    text("enemy " + this.index, this.x + 18, this.y + 20, 50, 50);
+    // Enemy body rect(x, y, sizex, sizey)
+    rect(this.x, this.y, 20, 20);
+  }
+
+  // Move relative to speed
+  move() {
+    // Check which way enemy is headed and update the speed
+    if (this.direction == 1) {
+      this.x = this.x - this.speed;
+    } else if (this.direction == 2) {
+      this.x = this.x + this.speed;
+    } else if (this.direction == 3) {
+      this.y = this.y - this.speed;
+    } else {
+      this.y = this.y + this.speed;
     }
   }
 }
-
-window.onresize = function() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;  
-  canvas.size(w,h);
-  width = w;
-  height = h;
-};
